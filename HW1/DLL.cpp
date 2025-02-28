bdaa347790ec649ff2294e5c19893bff79a1544b
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-02-28 21:37:27 +0500

    Update DLL.cpp

--- HW1/DLL.cpp
+++ HW1/DLL.cpp
@@ -14,21 +14,24 @@ DLL::~DLL() {
     while (temp != nullptr) {
         DLL_node* next = temp->next;
         delete temp;
+        cout << "Deleted DLL node" << endl;
         temp = next;
     }
 }
 
 void DLL::add_room(Room* room) {
+    //allocate memory for new node
     DLL_node* new_node = new DLL_node;
     new_node->room = room;
     new_node->next = nullptr;
     new_node->previous = nullptr;
 
+    // if list is empty, set head and tail to new node
     if (head == nullptr) {
         head = new_node;
         tail = new_node;
         current = new_node;
-    } else {
+    } else { // otherwise, add new node to end of list
         tail->next = new_node;
         new_node->previous = tail;
         tail = new_node;
