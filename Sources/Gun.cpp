876911d33a76aacfb434b52642f0d4c21534c2dd
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-04-29 19:59:51 +0500

    Delete Sources directory

--- Sources/Gun.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Headers/Gun.hpp"
-#include "../Headers/Weapon.hpp"
-#include <string>
-
-using namespace std;
-
-Gun::Gun(string name, int damage, int durability) : Weapon(name, damage, durability) {}
-
-Gun::~Gun() {}
